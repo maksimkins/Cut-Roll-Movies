3f552cc0365e2763749436f47262f64d5ba2717a
maksimkins <maks.aleshkov.04@gmail.com>
2025-07-30 13:02:59 +0400

    Change MovieVideo Repository

--- Cut-Roll-Movies/src/Cut-Roll-Movies.Core/MovieVideos/Repositories/IMovieVideoRepository.cs
+++ Cut-Roll-Movies/src/Cut-Roll-Movies.Core/MovieVideos/Repositories/IMovieVideoRepository.cs
@@ -1,10 +1,12 @@
+namespace Cut_Roll_Movies.Core.MovieVideos.Repositories;
+
 using Cut_Roll_Movies.Core.Common.Repositories.Interfaces;
 using Cut_Roll_Movies.Core.MovieVideos.Dtos;
-
-namespace Cut_Roll_Movies.Core.MovieVideos.Repositories;
+using Cut_Roll_Movies.Core.MovieVideos.Models;
 
 public interface IMovieVideoRepository : ICreateAsync<Guid, MovieVideoCreateDto>, IDeleteAsync<Guid, Guid>,
 IDeleteRangeById<bool, Guid>, IUpdateAsync<MovieVideoUpdateDto, Guid>
 {
-    
+    Task<IEnumerable<MovieVideo>> GetVideosByMovieIdAsync(Guid movieId);
+    Task<IEnumerable<MovieVideo>> GetVideosByTypeAsync(Guid movieId, string type);
 }
